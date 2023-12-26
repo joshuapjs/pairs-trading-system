aab0a5f343a5da72c377bd3f2366c4282f36e872
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-12-26 10:24:27 +0100

    Changing the infrastructure to one place_order function

--- tws_connector.py
+++ tws_connector.py
@@ -1,23 +1,25 @@
-from ib_insync import IB, Forex, Stock, LimitOrder
-
-ib = IB()
-ib.connect('127.0.0.1', 7497, clientId=0)
+from ib_insync import IB, LimitOrder, MarketOrder
 
 
-def forex_market_order(pair: str, action: str, amount: int, limit_price: float):
-    eur_usd_contract = Forex(pair, 'IDEALPRO')
-    eur_usd_contract_order = LimitOrder(action, amount, limit_price)
-    trade = ib.placeOrder(eur_usd_contract, eur_usd_contract_order)
+def place_order(asset, action: str, amount: int, limit_price: float = None):
+    """
+    This function will place a limit or market order on IBKR through the TWS.
+    :param asset: An instantiated asset class from the ib_insync library
+    :param action: "BUY" or "SELL".
+    :param amount: Amount of items of the asset.
+    :param limit_price: If specified the order will become a limit order, otherwise a
+    market order will be placed instead.
+    :return: Current trade logbook and the current order status.
+    """
+    # Connecting to the TWS through the local host.
+    ib = IB()
+    ib.connect('127.0.0.1', 7497, clientId=1)
+    # Creating a limit order if the limit order is specified and a market order of the
+    # limit order price remains unspecified.
+    eur_usd_contract_order = LimitOrder(action, amount, limit_price) \
+        if limit_price is not None else MarketOrder(action, amount)
+    # Placing the order specified order.
+    trade = ib.placeOrder(asset, eur_usd_contract_order)
+    # Giving the System time to process the order.
     ib.sleep(1)
-    assert trade in ib.trades()
-    return trade.log, trade.orderStatus.status
-
-
-def stock_market_order(symbol: str, limit_price: float, action: str, amount: int,
-                       currency: str = "EUR", exchange: str = "CBOE"):
-    current_stock = Stock(symbol, exchange, currency)
-    current_stock_order = LimitOrder(action, amount, limit_price)
-    trade = ib.placeOrder(current_stock, current_stock_order)
-    ib.sleep(1)
-    assert trade in ib.trades()
     return trade.log, trade.orderStatus.status
