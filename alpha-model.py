60bae487c69d1c4adda60a9b7b01ba18b0b16859
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-03-31 19:07:29 +0200

    data-connector.py added

--- alpha-model.py
+++ alpha-model.py
@@ -1,20 +1,6 @@
 import ib_insync
 
 
-class Asset:
-
-    def __init__(self,
-                 symbol: str,
-                 currency: str,
-                 exchange: str,
-                 current_quote: float):
-
-        self.symbol = symbol
-        self.currency = currency
-        self.exchange = exchange
-        self.quote = current_quote
-
-
 def generate_signal(asset_a: Asset, asset_b: Asset):
 
     delta = asset_a.quote - alpha - beta * asset_b.quote # TODO Model parameter ergänzen
