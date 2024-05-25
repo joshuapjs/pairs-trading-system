c1fce986005571173f366d42e32bd68c552088a1
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-05-25 22:58:34 +0200

    signal creation function added

--- _prototype.py
+++ _prototype.py
@@ -1,75 +1,81 @@
-import ib_insync
+from data_connector import Pair
 from itertools import chain, combinations
+import ib_insync
+import math
+import copy
 
 ib_insync.util.startLoop()
-# TODO: Bei Modularisierung muss ib bei den Funktionen als Parameter mitgegeben werden.
+# TODO: Bei Modularisierung muss ib bei den Funktionen als werden.
 ib = ib_insync.IB()
 
 # Connect to TWS.
 connection = ib.connect("127.0.0.1", 7497, clientId=15)
 
 
-def get_stock_data(pairs_trading_logic: dict):
-    recent_prices = dict()
+def get_stock_data(pairs_traded: list):
 
     # NOTE Comment this out for real time data. 
     ib.reqMarketDataType(3) 
     ib.sleep(1)
 
     # Transform the tickers into instances of the ib_insync Stock-class (Stock "Contracts").
-    contracts = [ib_insync.contract.Stock(ticker_a, "SMART", "USD"), ib_insync.contract.Stock(ticker_b, "SMART", "USD")
-                 for ticker_a, ticker_b in pairs_trading_logic.keys()]
+    for pair in pairs_traded:
+        # Define the contract - TODO make it dependend on the incoming stock - request the data of the stock remotely if possible and
+        # fill in the request correctly
+        pair.contract_a = ib_insync.contract.Stock(pair.ticker_a, "SMART", "USD")
+        pair.contract_b = ib_insync.contract.Stock(pair.ticker_b, "SMART", "USD")
+        # Defining the stock contracts alone is not sufficient. The Stock instances need to be qualified.
+        # in order to enable that data can be requested with them.
+        ib.qualifyContracts(pair.contract_a)
+        data_a = ib.reqMktData(pair.contract_a).dict()
+        ib.qualifyContracts(pair.contract_b)
+        data_b = ib.reqMktData(pair.contract_b).dict()
+        ib.sleep(2) # Maybe this is not necessary lets see
+        pair.quotes_a = data_a  # TODO Does this make sense of can I insert the request directly?
+        pair.quotes_b = data_b
 
-    # Defining the stock contracts alone is not sufficient. The Stock instances need to be qualified.
-    # in order to enable that data can be requested with them.
-    for ticker_a, ticker_b in contracts:
-        ib.qualifyContracts(ticker_a)
-        data_a = ib.reqMktData(ticker_a)
-        ib.qualifyContracts(ticker_b)
-        data_b = ib.reqMktData(ticker_b)
-        ib.sleep(1)
-        recent_prices[ticker_a.symbol] = (data_a.dict()["ask"], data_a.dict()["bid"])
-        recent_prices[ticker_b.symbol] = (data_b.dict()["ask"], data_b.dict()["bid"])
-
-    return recent_prices 
+    return pairs_traded
 
 
-def limit_order(limit_price: int, stock: str, exchange: str = "SMART", currency="USD", action="BUY", quantity=1000) :
-    # Instantiating the Stock contract.
-    contract = ib_insync.Stock(symbol=stock, 
-                                    exchange=exchange, 
-                                    currency=currency)
- 
+def stock_limit_order(contract: ib_insync.contract.Stock, limit_price: int, action="BUY", quantity=10) :
     order = ib_insync.LimitOrder(action, quantity, limit_price)
     ib.placeOrder(contract, order)
     ib.sleep(1)
 
 
-def market_order(stock: str, exchange: str = "SMART", currency="USD", action="BUY", quantity=1000) :
-    contract = ib_insync.Stock(symbol=stock, 
-                                    exchange=exchange, 
-                                    currency=currency)
- 
+def stock_market_order(contract: ib_insync.contract.Stock, action="BUY", quantity=10) :
     order = ib_insync.MarketOrder(action, quantity)
     ib.placeOrder(contract, order)
     ib.sleep(1)
 
 
-def compare_pairs(current_prices, pairs_trading_logic):  # Pairs trading logic ist the Research based insight about selected pairs
-
-    current_signals = dict()
+def compare_pairs(current_pairs):  # Pairs trading logic ist the Research based insight about selected pairs
     # Hier muss ein Threshold berechnet werden. Wo befinden wir uns,
-    # bei Betrachtung der stationären Zeitreihe die die Divergenz der Returns darstellt ? 
+    # bei Betrachtung der stationären Zeitreihe die die Divergenz der Returns darstellt ?
+    new_signals = dict()
+    for pair in current_pairs:
+        const, slope, threshold = pair.equation
+        estimate = const + slope * pair.quotes_b["ask"]
+        delta = pair.quotes_a["ask"] - estimate
+        if abs(delta) > threshold:
+            # assign priority based on strength of deviation TODO find a better way, maybe in terms of the distribution of the variable
+            deviation = (delta - threshold) / threshold
+            sign = math.copysign(1, delta)
+            new_signals[current_pairs.tickers] = (deviation, sign, pair, (copy.copy(pair.quote_a), copy.copy(pair.quote_b)))
+            # Das Portfolio Model muss dann basieredn auf der stärke der Abweichung und der Richtung, Entscheidungen treffen was gekauft werden soll.
+
+    return new_signals
 
 
 if __name__ == "__main__":
     # Just a quick functionality test
+    winning_pairs = [
+            Pair(("META", "MSFT"), "USD"),
+            Pair(("NVDA", "TSLA"), "USD")
+            ]
+    pairs = get_stock_data(winning_pairs)
+    print(pairs)
 
-    winning_pairs = {("AAPL","TSLA"):(None,None), ("MSFT","NVDA"): (None,None)} 
-    prices = get_stock_data(winning_pairs)
-    
-    for ticker in prices:
-        limit_order(limit_price[ticker.symbol], stock=ticker)
 else:
     raise ImportError("This module is not for external use. This is just a prototype.")
 
--- data_connector.py
+++ data_connector.py
@@ -1,8 +1,3 @@
-import polars
-import pandas as pd
-import numpy as np
-import ib_insync
-
 
 class Pair:
     """
@@ -16,65 +11,15 @@ class Pair:
 
         # TODO Prüfen ob es sich lohnen würde zwei unterschiedliche currencies zulassen.
 
-        long_ticker, short_ticker = tickers
+        a,b = tickers
 
-        self.tickers = tickers
-        self.long_ticker = long_ticker
-        self.short_ticker = short_ticker
-        self.long_quote = None
-        self.short_quote = None
-        self.currency = currency
+        self.tickers: tuple = tickers
+        self.ticker_a: str = a
+        self.ticker_b: str = b
+        self.contract_a: ib_insync.contract.Stock = None
+        self.contract_b: ib_insync.contract.Stock = None
+        self.quotes_a = None 
+        self.quotes_b = None
+        self.equation: tuple = None  # (const, slope, threshold)
+        self.currency: str = currency
     
-
-class Connection:
-    """
-    Build a connection to TWS and request data.
-    """
-    # NOTE: The current_pairs MUST be a dictionary mapping a frozenset containing both tickers on the contract.
-    # NOTE: Around the (Price)-Events produced by this class should the whole application be built.
-    # Setup of the client to request data from TWS.
-    def __init__(self, current_pairs: dict):
-        # A list containing the current Pairs that are traded.
-        self.current_pairs = current_pairs
-        self.current_tickers = set()  # Set that contains all current tickers, data is requested for.
-        self.current_stocks = list() # List containing the Stock class instances.
-
-    def connect(self):
-        ib_insync.util.startLoop()
-        ib = ib_insync.IB()
-
-        # Connect to TWS.
-        ib.connect("127.0.0.1", 7497, clientId=15)
-
-        # NOTE Comment this out for real time data. 
-        ib.reqMarketDataType(3) 
-        
-        ib.sleep(3)
-        # Unpacking instances of the Pair class.
-        for pair in list(self.current_pairs.values()):
-                self.current_tickers.add(pair.long_ticker)
-                self.current_tickers.add(pair.short_ticker)
-
-        # Transform the tickers into instances of the ib_insync Stock-class (Stock "Contracts").
-        contracts = [ib_insync.contract.Stock(current_ticker, "SMART", "USD") for current_ticker in list(self.current_tickers)]
-        
-        # We safe the contracts just created in a list that can be adjusted.
-        self.current_stocks = contracts
-        # Defining the stock contracts alone is not sufficient. The Stock instances need to be qualified.
-        # in order to enable that data can be requested with them.
-        for current_contract in self.current_stocks:
-            ib.qualifyContracts(current_contract)
-            data = ib.reqMktData(current_contract)
-            ib.sleep(1)
-            print(data.markprice)
-
-        # TODO: The Marketdata requested, has to update each relevant Pair instance. So that there are initial values in it.
-        # TODO: Maybe then there should be an almost endless function running (in a separated Procces or as main Process?)
-        #       that updates all the Pairs, resulting in a new price event. whenever a suffucient threshhold of such price events
-        #       is reached (every price event might be too much?) the calculation should run and trades placed etc.
-
-
-if __name__ == "__main__":
-    ticker_pair = Connection({frozenset(("AAPL", "TSLA")) : Pair(("AAPL", "TSLA"), "EUR")})
-    ib = ticker_pair.connect()
-
