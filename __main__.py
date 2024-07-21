da2c0f4047b781aee806db1650e82ed4cb6409f9
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-07-21 23:38:01 +0200

    modification of Portfolio.analyze signal

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
 from data_connector import Pair
 from itertools import chain, combinations
 from tws_connection import ib, build_connection
+import asyncio as aio
 import execution_model as em
 import ib_insync
 import math
 import copy
-
 
 # Check if a connection exists already
 if not ib.isConnected():
@@ -14,12 +14,10 @@ if not ib.isConnected():
 
 def connect_pairs(pairs_traded: list):
     # NOTE Comment this out for real time data.
-    ib.reqMarketDataType(3) 
-    ib.sleep(1)
+    ib.reqMarketDataType(3)
     # Transform the tickers into instances of the ib_insync Stock-class (Stock "Contracts").
     for pair in pairs_traded:
         pair.connect_data()
-        ib.sleep(1)
 
 
 if __name__ == "__main__":
@@ -28,7 +26,6 @@ if __name__ == "__main__":
                   Pair(("GM", "TSLA"), "USD"),
                   Pair(("AMZN", "CPNG"), "USD")]
 
-    # Connect to the data for each ticker.
     connect_pairs(test_pairs)
 
 else:
--- data_connector.py
+++ data_connector.py
@@ -3,8 +3,8 @@ This module contains the Pairs class to effectively access the data
 and related operations of each Pair that is traded.
 """
 import ib_insync
-from tws_connection import ib, build_connection 
-
+import asyncio as aio
+from tws_connection import ib, build_connection
 
 # Check if a connection exists already
 if not ib.isConnected():
@@ -22,32 +22,35 @@ class Pair:
     def __init__(self,
                  tickers: tuple,
                  currency: str):
-
         # TODO Prüfen ob es sich lohnen würde zwei unterschiedliche currencies zulassen.
 
-        a,b = tickers
+        a, b = tickers
 
         self.tickers: tuple = tickers
         self.ticker_a: str = a
         self.ticker_b: str = b
         self.contract_a: ib_insync.contract.Stock = None
         self.contract_b: ib_insync.contract.Stock = None
-        self.quotes_a = None 
+        self.quotes_a = None
         self.quotes_b = None
         self.equation: tuple = None  # (const, slope, threshold)
         self.currency: str = currency
 
-    def connect_data(self):
+    @staticmethod
+    def _collect_data(ticker, currency):
         # Define the contract - TODO make it dependend on the incoming stock - request the data of the stock remotely
-        #  if possible and fill in the request correctly
-        self.contract_a = ib_insync.contract.Stock(self.ticker_a, "SMART", "USD")
-        self.contract_b = ib_insync.contract.Stock(self.ticker_b, "SMART", "USD")
+        # if possible and fill in the request correctly
+        contract = ib_insync.contract.Stock(ticker, "SMART", currency)
         # Defining the stock contracts alone is not sufficient. The Stock instances need to be qualified.
         # in order to enable that data can be requested with them.
-        ib.qualifyContracts(self.contract_a)
-        data_a = ib.reqMktData(self.contract_a)
-        ib.qualifyContracts(self.contract_b)
-        data_b = ib.reqMktData(self.contract_b)
-        self.quotes_a = data_a  # TODO Does this make sense or can I insert the request directly?
-        self.quotes_b = data_b
+        ib.qualifyContracts(contract)
+        data = ib.reqMktData(contract)
+        return data, contract
 
+    def connect_data(self):
+        data_a, contract_a = self._collect_data(self.ticker_a, self.currency)
+        data_b, contract_b = self._collect_data(self.ticker_b, self.currency)
+        self.quotes_a = data_a
+        self.quotes_b = data_b
+        self.contract_a = contract_a
+        self.contract_b = contract_b
